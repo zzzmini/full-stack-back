6babf6b4998ce304d5d13b6848de6fcba469ace7
zzzmini <sm01079200887@gmail.com>
2025-03-26 18:09:48 +0900

    #4

--- src/main/java/com/my/backend/BackendApplication.java
+++ src/main/java/com/my/backend/BackendApplication.java
@@ -1,7 +1,10 @@
 package com.my.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +12,8 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
